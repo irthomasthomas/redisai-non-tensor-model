43d8960a053bb783bfdc60de4651216865727ae1
ir.thomasthomas@gmail.com <ir.thomasthomas@gmail.com>
2019-11-26 14:42:26 +0000

    Added runModel2 a workaround for redisAi on gears

--- example_rfc.py
+++ example_rfc.py
@@ -8,18 +8,23 @@ from gearsclient import GearsRemoteBuilder as GearsBuilder
 
 
 corpus = [
-    "The apple is on sale apple", "The oranges are on orange sale",
-    "The apple and is present", "The orange and is present"]
+    "The apple is on sale apple", "The orange are on sale",
+    "The apple is present", "The orange is present",
+    "the best fruit is apple", "best fruit is orange",
+    "apple is my favourite fruit", "orange are my favourite",
+    "apple are the best", "orange are the best",
+    "an apple a day keeps the grocer in pay", "orange a day keeps the doc away"]
 
-Y = np.array([0,1,0,1])
+Y = np.array([0,1,0,1,0,1,0,1,0,1,0,1]) # segfault on apple and orange
+# Y = np.array([1,0,1,0,1,0,1,0,1,0,1,0]) # segfault on apple
 vectorizer = CountVectorizer(min_df=1)
 X = vectorizer.fit_transform(corpus).toarray()
-pickle.dump(vectorizer, open("countVectorizer.pickle", "wb"))
+pickle.dump(vectorizer, open("/root/dev/projects/redisai-non-tensor-model/countVectorizer.pickle", "wb"))
 
 classifier = RandomForestClassifier()
 classifier.fit(X, Y)
 
-sample_text = "orange is present"
+sample_text = "apple is present"
 print(sample_text)
 sample = vectorizer.transform(
         [sample_text]).toarray()
@@ -27,16 +32,20 @@ sample = vectorizer.transform(
 pred = classifier.predict(sample)
 print("rfc model prediction...")
 print(pred)
+print("SHAPE")
+print(X.shape) # 12, 22
+########################################
 
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType
-initial_type = [("float_input", FloatTensorType([1, 10]))]
+initial_type = [("float_input", FloatTensorType([1, 20]))]
 onx = convert_sklearn(classifier, initial_types=initial_type)
 with open("rfc_onx.onnx", "wb") as f:
         f.write(onx.SerializeToString())
-
+print("saved onx")
 import onnxruntime as rt
 sess = rt.InferenceSession("rfc_onx.onnx")
+
 input_name = sess.get_inputs()[0].name
 label_name = sess.get_outputs()[0].name
 pred_onx = sess.run(
@@ -44,22 +53,41 @@ pred_onx = sess.run(
 print("onnx prediction...")
 print(pred_onx)
 
+########################################
+
 conn = redis.Redis(host="localhost", port=6379, db=0)
 with open("rfc_onx.onnx", "rb") as f:
     model = f.read()
     res = conn.execute_command('AI.MODELSET', 'sklmodel', 'ONNX', 'CPU', model)
-
 rai = redisai.Client()
-tensor = redisai.BlobTensor.from_numpy(
-    sample.astype(np.float32))
-rai.tensorset('tensor', tensor)
-rai.modelrun('sklmodel', inputs=['tensor'], outputs=['out_label', 'out_probs'])
+rai.tensorset('tensor', sample.astype(np.float32))
+rai.modelrun(
+    'sklmodel', inputs=['tensor'], outputs=['out_label', 'out_probs'])
 out = rai.tensorget('out_label')
 print("RedisAI prediction...")
 print(out)
 
+#########################################
+
+ba = np.asarray(sample, dtype=np.float32)
+conn.execute_command(
+    'AI.TENSORSET', 'tensor', 'FLOAT', '1', '20', 'BLOB', ba.tobytes())
+conn.execute_command('AI.MODELRUN', 'sklmodel', 'INPUTS', 'tensor', 'OUTPUTS', 'out_label', 'out_probs')
+out = conn.execute_command('AI.TENSORGET', 'out_label', 'VALUES')
+print(out[2])
+
+##########################################
+
+initialized_key = '{}:initialized'.format('xIn')
+# Check if this Redis instance had already been initialized
+initialized = conn.exists(initialized_key)
+if initialized:
+    print('Discovered evidence of a privious initialization - skipping.')
+    exit(0)
 print("Load gear")
 with open('rfc_gear.py', 'rb') as f:
     gear = f.read()
     res = conn.execute_command('RG.PYEXECUTE', gear)
     print(res)
+print('Flag initialization as done - ', end='') 
+print(conn.set(initialized_key, 'most certainly.'))
--- rfc_gear.py
+++ rfc_gear.py
@@ -2,39 +2,72 @@ import redisAI as rai
 # from sklearn.feature_extraction.text import CountVectorizer
 import pickle
 import numpy as np
+import redis
 
+conn = redis.Redis(host="localhost", port=6379, db=0)
+print(str(conn))
 
 def runModel(x):
+    print("runModel")
+    print(str(x))
     vectorizer = pickle.load(open('/root/dev/projects/redisai-non-tensor-model/countVectorizer.pickle', 'rb'))
-
-    text_sample = vectorizer.transform(
+    sample = vectorizer.transform(
         [x['text']]).toarray()
-
-    modelRunner = rai.createModelRunner('sklmodel')
-
-    ba = np.asarray(text_sample, dtype=np.float32)
+    # ba = np.asarray(sample, dtype=np.float32)
+    # conn.execute_command(
+    #     'AI.TENSORSET', 'tensor', 'FLOAT', '1', '20', 'BLOB', ba.tobytes())
+    ba = np.asarray(sample, dtype=np.float32)
+    # ba = ba.tobytes()
     ba = bytearray(ba.tobytes())
+    print("bytes")
 
-    tensor = rai.createTensorFromBlob('FLOAT', [1, 10], ba)
+    tensor = rai.createTensorFromBlob('FLOAT', [1, 20], ba)
+    print("tensor")
+    modelRunner = rai.createModelRunner('sklmodel')
+    print("modelrunner")
     rai.modelRunnerAddInput(modelRunner, 'float_input', tensor)
+    print("add input")
+    # rai.modelRunnerAddInput(modelRunner, 'float_input', sample.astype(np.float32))
+
     rai.modelRunnerAddOutput(modelRunner, 'output_label')
     rai.modelRunnerAddOutput(modelRunner, 'output_probability')
-    print(x['text'])
+    print("add output")
 
-    # SEG FAULTS HERE
-    # model_replies = rai.modelRunnerRun(modelRunner)
-
-    model_output = model_replies[0]
+    # print(x['text'])
+    model_replies = rai.modelRunnerRun(modelRunner)
+    # ERROR: type 0 is not supported in this function
+    print("model replies")
+    # model_output = model_replies[0]
     print("runModel output...")
-    print(model_output)
+    print(str(model_replies)) # = None
 
 def storeResults(x):
     print(str(x))
-    res = execute('XADD', 'xOut', 'MAXLEN', '~', 1000, '*', 'streamId', x['streamId'], 'text', x['text'], 'pred', x['output_label'])
+    res = execute(
+        'XADD', 'xOut', 'MAXLEN', '~', 1000, '*',
+        'streamId', x['streamId'], 'text', x['text'],
+        'pred', x['output_label'])
     print(str(res))
     
+def runModel2(x):
+    vectorizer = pickle.load(open('/root/dev/projects/redisai-non-tensor-model/countVectorizer.pickle', 'rb'))
+    sample = vectorizer.transform(
+        [x['text']]).toarray()
+    ba = np.asarray(sample, dtype=np.float32)
+    conn.execute_command(
+        'AI.TENSORSET', 'tensor', 'FLOAT', '1', '20', 'BLOB', ba.tobytes())
+    conn.execute_command(
+        'AI.MODELRUN', 'sklmodel', 'INPUTS', 'tensor', 'OUTPUTS', 'out_label', 'out_probs')
+    out = conn.execute_command(
+        'AI.TENSORGET', 'out_label', 'VALUES')
+    print(out[2])
+
+
+# TODO: Run model through normal redis client
 bg = GearsBuilder('StreamReader')
 bg.foreach(lambda x: print(str(x['streamId'])))
-bg.foreach(runModel)
-bg.foreach(storeResults)
+# bg.foreach(runModel)
+bg.foreach(runModel2)
+# bg.foreach(storeResults)
 bg.register('xIn')
+

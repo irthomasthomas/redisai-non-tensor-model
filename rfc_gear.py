52a3157003f4678c49eed16bf121b4fcd3627dca
ir.thomasthomas@gmail.com <ir.thomasthomas@gmail.com>
2019-11-27 20:39:38 +0000

    - rfc_gear.py Added runModel2 to test running model via redis conn

--- rfc_gear.py
+++ rfc_gear.py
@@ -6,37 +6,18 @@ import redis
 
 conn = redis.Redis(host="localhost", port=6379, db=0)
 print(str(conn))
+vectorizer = pickle.load(open('/root/dev/projects/redisai-non-tensor-model/countVectorizer.pickle', 'rb'))
 
 def runModel(x):
-    print("runModel")
-    print(str(x))
-    vectorizer = pickle.load(open('/root/dev/projects/redisai-non-tensor-model/countVectorizer.pickle', 'rb'))
     sample = vectorizer.transform(
         [x['text']]).toarray()
-    # ba = np.asarray(sample, dtype=np.float32)
-    # conn.execute_command(
-    #     'AI.TENSORSET', 'tensor', 'FLOAT', '1', '20', 'BLOB', ba.tobytes())
     ba = np.asarray(sample, dtype=np.float32)
-    # ba = ba.tobytes()
-    ba = bytearray(ba.tobytes())
-    print("bytes")
-
-    tensor = rai.createTensorFromBlob('FLOAT', [1, 20], ba)
-    print("tensor")
     modelRunner = rai.createModelRunner('sklmodel')
-    print("modelrunner")
-    rai.modelRunnerAddInput(modelRunner, 'float_input', tensor)
-    print("add input")
-    # rai.modelRunnerAddInput(modelRunner, 'float_input', sample.astype(np.float32))
-
+    rai.modelRunnerAddInput(modelRunner, 'float_input', ba)
     rai.modelRunnerAddOutput(modelRunner, 'output_label')
     rai.modelRunnerAddOutput(modelRunner, 'output_probability')
-    print("add output")
-
-    # print(x['text'])
     model_replies = rai.modelRunnerRun(modelRunner)
-    # ERROR: type 0 is not supported in this function
-    print("model replies")
+        # ERROR: type 0 is not supported in this function
     # model_output = model_replies[0]
     print("runModel output...")
     print(str(model_replies)) # = None
@@ -50,7 +31,6 @@ def storeResults(x):
     print(str(res))
     
 def runModel2(x):
-    vectorizer = pickle.load(open('/root/dev/projects/redisai-non-tensor-model/countVectorizer.pickle', 'rb'))
     sample = vectorizer.transform(
         [x['text']]).toarray()
     ba = np.asarray(sample, dtype=np.float32)
@@ -65,9 +45,7 @@ def runModel2(x):
 
 # TODO: Run model through normal redis client
 bg = GearsBuilder('StreamReader')
-bg.foreach(lambda x: print(str(x['streamId'])))
-# bg.foreach(runModel)
-bg.foreach(runModel2)
+bg.foreach(runModel)
 # bg.foreach(storeResults)
 bg.register('xIn')
 
